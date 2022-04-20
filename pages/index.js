b16f8533dc2b476525f2876d28c77acd31398fb5
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-20 21:17:59 +0700

    updating pages index / updating the banner

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,35 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
-const Banner = ({ purpose }) => (
+const Banner = ({
+	purpose,
+	title1,
+	title2,
+	desc1,
+	desc2,
+	buttonText,
+	linkName,
+	imageUrl,
+}) => (
 	<Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
 		<Image src={imageUrl} width={500} height={300} alt="banner" />
 		<Box p="5">
 			<Text color="gray.500" fontSize="sm" fontWeight="medium">
 				{purpose}
 			</Text>
+			<Text fontSize="3xl" fontWeight="bold">
+				{title1}
+				<br />
+				{title2}
+			</Text>
+			<Text fontSize="lg" paddingTop="3" paddingBottom="3" color="gray.700">
+				{desc1}
+				<br />
+				{desc2}
+			</Text>
+			<button fontSize="xl" bg="blue.300" color="white">
+				<Link href={linkName}>{buttonText}</Link>
+			</button>
 		</Box>
 	</Flex>
 );
