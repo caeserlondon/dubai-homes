79ff301d56b162e66d72fedd0b0b4640fbe63835
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-20 20:56:58 +0700

    updating pages index / adding the banner

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,18 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
+const Banner = ({ purpose }) => (
+	<Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+		{purpose}
+	</Flex>
+);
+
 export default function Home() {
-	return <div>Dubai Homes</div>;
+	return (
+		<div>
+			<h1>Dubai Homes</h1>
+			<Banner purpose={"For Sale"} />
+			<Banner purpose={"For Rent"} />
+		</div>
+	);
 }
