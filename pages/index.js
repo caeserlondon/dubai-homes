ea053b2975458bc79138cc65b41c049c50aa82cc
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-21 08:19:38 +0700

    updating pages index & next.config.js

--- pages/index.js
+++ pages/index.js
@@ -39,8 +39,26 @@ export default function Home() {
 	return (
 		<div>
 			<h1>Dubai Homes</h1>
-			<Banner purpose={"For Sale"} />
-			<Banner purpose={"For Rent"} />
+			<Banner
+				purpose="RENT A HOME"
+				title1="Reantal Homes for"
+				title2="Everyone"
+				desc1="Explore Apartments, Villas, Homes"
+				desc2="And More"
+				buttonText="Explore Renting"
+				linkName="/search?porpose=for-rent"
+				imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+			/>
+			<Banner
+				purpose="BUY A HOME"
+				title1="Find, Buy & Own Your"
+				title2="Dream Home"
+				desc1="Explore Apartments, Villas, Homes"
+				desc2="And More"
+				buttonText="Explore Buying"
+				linkName="/search?porpose=for-sale"
+				imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
+			/>
 		</div>
 	);
 }
