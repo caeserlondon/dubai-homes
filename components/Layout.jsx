d62c9ddb476319d3f1c7401f9c12798c5929a1f9
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-23 17:51:40 +0700

    adding the footer component to the Layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Box } from "@chakra-ui/react";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => (
 	<>
@@ -12,7 +13,9 @@ const Layout = ({ children }) => (
 				<Navbar />
 			</header>
 			<main>{children}</main>
-			<footer>Footer</footer>
+			<footer>
+				<Footer />
+			</footer>
 		</Box>
 	</>
 );
