5348904a2bce039f11b55edf6632f4df5a9c05c2
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-23 09:36:03 +0700

    importing the Chakra components to the app api

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import Router from "next/router";
+import Head from "next/head";
+import Nprogress from "nprogress";
+import { chakraProvider } from "@chakra-ui/react";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
