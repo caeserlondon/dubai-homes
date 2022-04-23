d78f07e7d5eca28852f5e106cbf41f0e7570d5e9
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-23 17:20:12 +0700

    adding the Navbar component

---
+++ components/Navbar.jsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import {
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	IconButton,
+	Flex,
+	Box,
+	Spacer,
+} from "@chakra-ui/react";
+import { FcMenu, FcHome, FcAbout } from "react-icons/fc";
+import { BsSearch } from "react-icons/bs";
+import { FiKey } from "react-icons/fi";
+
+const Navbar = () => (
+	<Flex p="2" borderBottom="1px" borderColor="yellow.600">
+		<Box fontSize="3xl" color="yellow.500" fontWeight="bold">
+			<Link href="/" paddingLeft="2">
+				Dubai Homes
+			</Link>
+		</Box>
+		<Spacer />
+		<Box>
+			<Menu>
+				<MenuButton
+					as={IconButton}
+					icon={<FcMenu />}
+					variant="outlined"
+					color="yellow.400"
+					borderColor="yellow.500"
+				/>
+				<MenuList>
+					<Link href="/" passHref>
+						<MenuItem icon={<FcHome />}>Home</MenuItem>
+					</Link>
+					<Link href="/search" passHref>
+						<MenuItem icon={<BsSearch />}>Search</MenuItem>
+					</Link>
+					<Link href="/search?porpuse=for-sale" passHref>
+						<MenuItem icon={<FcAbout />}>Buy A Property</MenuItem>
+					</Link>
+					<Link href="/search?porpuse=for-rent" passHref>
+						<MenuItem icon={<FiKey />}>Rent A Property</MenuItem>
+					</Link>
+				</MenuList>
+			</Menu>
+		</Box>
+	</Flex>
+);
+export default Navbar;
