eb11a0ae78dc81b7511642075b2aed0c960c4cd9
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-23 17:50:44 +0700

    adding the footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+
+const Footer = () => (
+	<Box
+		textAlign="center"
+		p="5"
+		color="yellow.600"
+		borderTop="1px"
+		borderColor="yellow.600"
+	>
+		@2022 DUBAI HOMES INC.
+	</Box>
+);
+export default Footer;
