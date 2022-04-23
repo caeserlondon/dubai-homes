6a3c9af71fc9ba37d1dce73298d141c880dd86f5
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-23 09:44:24 +0700

    wrapping the layout with Chakra provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
 import Router from "next/router";
 import Head from "next/head";
 import Nprogress from "nprogress";
-import { chakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, chakraProvider } from "@chakra-ui/react";
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head></Head>
+			<ChakraProvider>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</ChakraProvider>
+		</>
+	);
 }
 
 export default MyApp;
