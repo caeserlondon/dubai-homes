039efb027998e95075c8812b38fd50ad4d2a3f76
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-24 18:26:40 +0700

    updating the search filters component

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -14,9 +14,35 @@ import { MdCancel } from "react-icons/md";
 import Image from "next/image";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
 import noresult from "../assets/images/noresult.svg";
+import { filterData, getFilterValues } from "../utils/filterData";
 
 const SearchFilters = () => {
-	return <div>searchFilters</div>;
+	const [filters, setFilters] = useState(filterData);
+
+	const searchProperties = (filterValues) => {};
+
+	return (
+		<Flex bg="yellow.100" p="4" justifyContent="center" flexWrap="wrap">
+			{filters.map((filter) => (
+				<Box key={filter.queryName}>
+					<Select
+						placeholder={filter.placeholder}
+						w="fit-content"
+						p="2"
+						onChange={(e) =>
+							searchProperties({ [filter.queryName]: e.target.value })
+						}
+					>
+						{filter?.items?.map((item) => (
+							<option value={item.value} key={item.value}>
+								{item.name}
+							</option>
+						))}
+					</Select>
+				</Box>
+			))}
+		</Flex>
+	);
 };
 
 export default SearchFilters;
