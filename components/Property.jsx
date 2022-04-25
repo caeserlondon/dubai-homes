5bb02424929ab4016b41608d5f5256a343fc4b5c
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-25 14:28:36 +0700

    updating the Property component

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/layout";
 import { Avatar } from "@chakra-ui/avatar";
 import { FaBed, FaBath } from "react-icons/fa";
 import { BsGridFill } from "react-icons/bs";
