f78273643eec95e9ed5f74eb71c2ef9029f86b2e
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-25 21:00:04 +0700

    updating the layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,22 +1,24 @@
 import Head from "next/head";
 import { Box } from "@chakra-ui/react";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
 
-const Layout = ({ children }) => (
-	<>
-		<Head>
-			<title>Dubai Homes</title>
-		</Head>
-		<Box maxWidth="1280px" m="auto">
-			<header>
-				<Navbar />
-			</header>
-			<main>{children}</main>
-			<footer>
-				<Footer />
-			</footer>
-		</Box>
-	</>
-);
-export default Layout;
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+export default function Layout({ children }) {
+	return (
+		<>
+			<Head>
+				<title>Dubai Homes</title>
+			</Head>
+			<Box maxWidth="1280px" m="auto">
+				<header>
+					<Navbar />
+				</header>
+				<main>{children}</main>
+				<footer>
+					<Footer />
+				</footer>
+			</Box>
+		</>
+	);
+}
