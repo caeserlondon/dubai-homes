ec8a881239453a50732faa3e6039cebc41d8757e
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-25 21:51:12 +0700

    updating the fetch API utility file

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -2,14 +2,12 @@ import axios from "axios";
 
 export const baseUrl = "https://bayut.p.rapidapi.com";
 
-// headers: {
-//   'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
-//   'X-RapidAPI-Key': '79e781e850msha37d0c7ef349fadp1996cajsn07158782356d'
-// }
-
 export const fetchApi = async (url) => {
 	const { data } = await axios.get(url, {
-		headers: {},
+		headers: {
+			"X-RapidAPI-Host": "bayut.p.rapidapi.com",
+			"X-RapidAPI-Key": "YOUR_API_KEY_HERE",
+		},
 	});
 	return data;
 };
