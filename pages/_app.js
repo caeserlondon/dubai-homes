c3f5b58373e39cbc0235ca400fc086333eb96540
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2025-11-03 18:45:32 +0700

    cleaning up

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,18 @@
-import Router from "next/router";
-import Head from "next/head";
-import NProgress from "nprogress";
-import { ChakraProvider } from "@chakra-ui/react";
-import Layout from "../components/Layout";
+import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import Layout from '../components/Layout';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
 	NProgress.configure({ showSpinner: false });
 
-	Router.events.on("routeChangeStart", () => {
+	Router.events.on('routeChangeStart', () => {
 		NProgress.start();
 	});
 
-	Router.events.on("routeChangeComplete", () => {
+	Router.events.on('routeChangeComplete', () => {
 		NProgress.done();
 	});
 
@@ -19,11 +20,11 @@ function MyApp({ Component, pageProps }) {
 		<>
 			<Head>
 				<link
-					rel="stylesheet"
-					href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
-					integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
-					crossOrigin="anonymous"
-					referrerPolicy="no-referrer"
+					rel='stylesheet'
+					href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css'
+					integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=='
+					crossOrigin='anonymous'
+					referrerPolicy='no-referrer'
 				/>
 			</Head>
 			<ChakraProvider>
