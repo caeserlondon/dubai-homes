3822cf75c8a805758ffc101b2fdb3ee6417b16fb
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2025-11-03 18:44:14 +0700

    updating the navigations buttons

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,23 +1,42 @@
-import Link from "next/link";
 import {
+	Box,
+	Flex,
+	IconButton,
 	Menu,
 	MenuButton,
-	MenuList,
 	MenuItem,
-	IconButton,
-	Flex,
-	Box,
+	MenuList,
 	Spacer,
-} from "@chakra-ui/react";
-import { FcMenu, FcHome, FcAbout } from "react-icons/fc";
-import { BsSearch } from "react-icons/bs";
-import { FiKey } from "react-icons/fi";
+} from '@chakra-ui/react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { BsSearch } from 'react-icons/bs';
+import { FcAbout, FcHome, FcMenu } from 'react-icons/fc';
+import { FiKey } from 'react-icons/fi';
 
 const Navbar = () => (
-	<Flex p="2" borderBottom="1px" borderColor="yellow.600">
-		<Box fontSize="3xl" color="yellow.500" fontWeight="bold">
-			<Link href="/" paddingLeft="2">
+	<Flex p='2' borderBottom='1px' borderColor='yellow.600'>
+		<Box fontSize='3xl' color='yellow.500' fontWeight='bold'>
+			<Link href='/' paddingLeft='2'>
 				Dubai Homes
+			</Link>
+		</Box>
+		<Spacer />
+		<Box fontSize='3xl' color='yellow.500' fontWeight='bold'>
+			<Link href='/search' paddingLeft='2'>
+				Search
+			</Link>
+		</Box>
+		<Spacer />
+		<Box fontSize='3xl' color='yellow.500' fontWeight='bold'>
+			<Link href='/search?purpose=for-sale' paddingLeft='2'>
+				Buy A Property
+			</Link>
+		</Box>
+		<Spacer />
+		<Box fontSize='3xl' color='yellow.500' fontWeight='bold'>
+			<Link href='/search?purpose=for-rent' paddingLeft='2'>
+				Rent A Property
 			</Link>
 		</Box>
 		<Spacer />
@@ -26,21 +45,21 @@ const Navbar = () => (
 				<MenuButton
 					as={IconButton}
 					icon={<FcMenu />}
-					variant="outlined"
-					color="yellow.400"
-					borderColor="yellow.500"
+					variant='outlined'
+					color='yellow.400'
+					borderColor='yellow.500'
 				/>
 				<MenuList>
-					<Link href="/" passHref>
+					<Link href='/' passHref>
 						<MenuItem icon={<FcHome />}>Home</MenuItem>
 					</Link>
-					<Link href="/search" passHref>
+					<Link href='/search' passHref>
 						<MenuItem icon={<BsSearch />}>Search</MenuItem>
 					</Link>
-					<Link href="/search?purpose=for-sale" passHref>
+					<Link href='/search?purpose=for-sale' passHref>
 						<MenuItem icon={<FcAbout />}>Buy A Property</MenuItem>
 					</Link>
-					<Link href="/search?purpose=for-rent" passHref>
+					<Link href='/search?purpose=for-rent' passHref>
 						<MenuItem icon={<FiKey />}>Rent A Property</MenuItem>
 					</Link>
 				</MenuList>
