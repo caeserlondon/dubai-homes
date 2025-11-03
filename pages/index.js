d0c06a1b617ff66c13ccc4b930d969780cf13e64
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2025-11-03 18:46:08 +0700

    cleaning up

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Link from "next/link";
-import Image from "next/image";
-import { Flex, Box, Text, Button } from "@chakra-ui/react";
-import Property from "../components/property";
-import { baseUrl, fetchApi } from "../utils/fetchApi";
-import buy from "../assets/images/buy.jpg";
-import rent from "../assets/images/rent.jpg";
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
+import Image from 'next/image';
+import Link from 'next/link';
+import buy from '../assets/images/buy.jpg';
+import rent from '../assets/images/rent.jpg';
+import Property from '../components/Property';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
 export const Banner = ({
 	purpose,
 	title1,
@@ -15,23 +15,23 @@ export const Banner = ({
 	linkName,
 	imageUrl,
 }) => (
-	<Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
-		<Image src={imageUrl} width={500} height={300} alt="banner" />
-		<Box p="5">
-			<Text color="yellow.600" fontSize="sm" fontWeight="medium">
+	<Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
+		<Image src={imageUrl} width={500} height={300} alt='banner' />
+		<Box p='5'>
+			<Text color='yellow.600' fontSize='sm' fontWeight='medium'>
 				{purpose}
 			</Text>
-			<Text fontSize="3xl" fontWeight="bold" color="yellow.500">
+			<Text fontSize='3xl' fontWeight='bold' color='yellow.500'>
 				{title1}
 				<br />
 				{title2}
 			</Text>
-			<Text fontSize="lg" paddingTop="3" paddingBottom="3" color="yellow.600">
+			<Text fontSize='lg' paddingTop='3' paddingBottom='3' color='yellow.600'>
 				{desc1}
 				<br />
 				{desc2}
 			</Text>
-			<Button fontSize="xl" colorScheme="yellow" variant="outline">
+			<Button fontSize='xl' colorScheme='yellow' variant='outline'>
 				<Link href={linkName}>{buttonText}</Link>
 			</Button>
 		</Box>
@@ -41,31 +41,31 @@ export const Banner = ({
 const Home = ({ propertiesForSale, propertiesForRent }) => (
 	<Box>
 		<Banner
-			purpose="RENT A HOME"
-			title1="Reantal Homes for"
-			title2="Everyone."
-			desc1="Explore Apartments, Villas, Homes"
-			desc2="And More..."
-			buttonText="Explore Renting"
-			linkName="/search?porpose=for-rent"
+			purpose='RENT A HOME'
+			title1='Reantal Homes for'
+			title2='Everyone.'
+			desc1='Explore Apartments, Villas, Homes'
+			desc2='And More...'
+			buttonText='Explore Renting'
+			linkName='/search?porpose=for-rent'
 			imageUrl={rent}
 		/>
-		<Flex flexWrap="wrap">
+		<Flex flexWrap='wrap'>
 			{propertiesForRent.map((property) => (
 				<Property property={property} key={property.id} />
 			))}
 		</Flex>
 		<Banner
-			purpose="BUY A HOME"
-			title1="Find, Buy & Own Your"
-			title2="Dream Home."
-			desc1="Explore Apartments, Villas, Homes"
-			desc2="And More..."
-			buttonText="Explore Buying"
-			linkName="/search?porpose=for-sale"
+			purpose='BUY A HOME'
+			title1='Find, Buy & Own Your'
+			title2='Dream Home.'
+			desc1='Explore Apartments, Villas, Homes'
+			desc2='And More...'
+			buttonText='Explore Buying'
+			linkName='/search?porpose=for-sale'
 			imageUrl={buy}
 		/>
-		<Flex flexWrap="wrap">
+		<Flex flexWrap='wrap'>
 			{propertiesForSale.map((property) => (
 				<Property property={property} key={property.id} />
 			))}
