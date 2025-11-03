ee6f4655c1a8043378e761a2926a5a22803ad0f1
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2025-11-03 15:31:02 +0700

    adding the api keys

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,12 +1,12 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const baseUrl = "https://bayut.p.rapidapi.com";
+export const baseUrl = 'https://bayut.p.rapidapi.com';
 
 export const fetchApi = async (url) => {
 	const { data } = await axios.get(url, {
 		headers: {
-			"X-RapidAPI-Host": "bayut.p.rapidapi.com",
-			"X-RapidAPI-Key": "YOUR_API_KEY_HERE",
+			'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
+			'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
 		},
 	});
 	return data;
